b64ee35f55bd87eb3ac269fac6797772d573f745
sandeepsinghh143 <sandeepsinghh143@gmail.com>
2024-05-13 21:07:39 +0530

    updated2

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const BlogCard = ({blog}) => {
   return (
-    <div className="p-8 border border-black rounded-2xl flex flex-col gap-10 w-4/5 m-auto min-h-[500px] dark:bg-[#1a1c1f]">
+    <div className="p-8 border border-black rounded-2xl flex flex-col gap-10 m-auto min-h-[500px] dark:bg-[#1a1c1f]">
       <div className="text-2xl">{blog.date}</div>
       <div className="text-4xl">{blog.title}</div>
       <div
