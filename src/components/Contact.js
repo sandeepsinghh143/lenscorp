f6f8f4142642963d38324e50c3f7a03f88acb927
sandeepsinghh143 <sandeepsinghh143@gmail.com>
2024-05-13 23:08:57 +0530

    updated6

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,9 +7,9 @@ const Contact = () => {
         <div className='lg:w-1/2 w-[90%] flex flex-col justify-center gap-10 xl:pl-40 p-4'>
             <div className='text-7xl w-8/12'>Get in touch with us</div>
             <div className='w-fit text-2xl'>Send your enquiry now!</div>
-            <div className='flex justify-between bg-[#ecedec] dark:bg-black p-1 md:pl-8 rounded-full'>
-                <input type="email" name="email" id="" placeholder='Enter email address' className=' bg-transparent border-0 outline-0 p-3 text-base text-[#887f95]'/>
-                <button className='bg-[#262f5c] text-white py-3 px-8 rounded-full'>Request Demo</button>
+            <div className='max-w-fit flex justify-between bg-[#ecedec] dark:bg-black p-1 md:pl-8 rounded-full'>
+                <input type="email" name="email" id="" placeholder='Enter email address' className='bg-transparent border-0 outline-0 md:p-3 md:text-base text-sm text-[#887f95]'/>
+                <button className='bg-[#262f5c] text-white md:py-3 md:px-8 p-2 rounded-full'>Request Demo</button>
             </div>
         </div>
         <div className='lg:w-1/2 w-[90%] m-auto h-[600px] flex justify-center items-center dark:bg-black'>
